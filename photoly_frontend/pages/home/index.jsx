de04361d6b6f1d6a39620844346513fbf7bdb2ae
Slijeff <jeff83636tx@gmail.com>
2022-06-26 23:15:53 -0500

    feat: add photo display

--- photoly_frontend/pages/home/index.jsx
+++ photoly_frontend/pages/home/index.jsx
@@ -1,6 +1,6 @@
 import Navbar from "../../components/Navbar"
 import Sidebar from "../../components/Sidebar";
-import {Heading, HStack***REMOVED*** from "@chakra-ui/react";
+import {HStack***REMOVED*** from "@chakra-ui/react";
 import Pathbar from "../../components/Pathbar";
 
 export default function Home() {
@@ -8,7 +8,7 @@ export default function Home() {
     return (
         <>
             <Navbar></Navbar>
-            <HStack>
+            <HStack alignItems={"flex-start"***REMOVED***>
                 <Sidebar/>
                 <Pathbar></Pathbar>
             </HStack>
