87badebcedb23780b046ada9425aa9a622291bb3
Slijeff <jeff83636tx@gmail.com>
2022-06-26 23:15:53 -0500

    feat: add photo display

--- photoly_frontend/components/Pathbar.jsx
+++ photoly_frontend/components/Pathbar.jsx
@@ -1,15 +1,33 @@
-import {Divider, Heading, HStack, Icon, Text, VStack***REMOVED*** from "@chakra-ui/react";
+import {Box, Divider, Flex, HStack, Icon, Image, Text, VStack***REMOVED*** from "@chakra-ui/react";
 import {BiChevronRight***REMOVED*** from "react-icons/bi";
 
 export default function Pathbar(){
     return (
-        <VStack alignSelf={"flex-start"***REMOVED*** pt={8***REMOVED*** pl={8***REMOVED*** w={"100%"***REMOVED*** pr={8***REMOVED***>
+        <VStack pt={8***REMOVED*** pl={8***REMOVED*** w={"100%"***REMOVED*** pr={8***REMOVED***>
             <HStack alignSelf={"flex-start"***REMOVED***>
                 <Icon as={BiChevronRight***REMOVED*** boxSize={8***REMOVED*** color={"gray.500"***REMOVED***/>
                 <Text fontWeight={"bold"***REMOVED*** color={"gray.500"***REMOVED***>/home</Text>
             </HStack>
             <Divider/>
-            <Heading>Photo Here</Heading>
+            <Flex w={"100%"***REMOVED*** justifyContent={"flex-start"***REMOVED*** wrap={"wrap"***REMOVED***>
+                <Box maxW='sm' overflow='hidden' p={2***REMOVED***>
+                    <Image src={"https://bit.ly/2Z4KKcF"***REMOVED*** alt={"image"***REMOVED*** borderRadius='lg'/>
+                </Box>
+                <Box maxW='sm' overflow='hidden' p={2***REMOVED***>
+                    <Image src={"https://bit.ly/2Z4KKcF"***REMOVED*** alt={"image"***REMOVED*** borderRadius='lg'/>
+                </Box>
+                <Box maxW='sm' overflow='hidden' p={2***REMOVED***>
+                    <Image src={"https://bit.ly/2Z4KKcF"***REMOVED*** alt={"image"***REMOVED*** borderRadius='lg'/>
+                </Box>
+                <Box maxW='sm' overflow='hidden' p={2***REMOVED***>
+                    <Image src={"https://bit.ly/2Z4KKcF"***REMOVED*** alt={"image"***REMOVED*** borderRadius='lg'/>
+                </Box>
+                <Box maxW='sm' overflow='hidden' p={2***REMOVED***>
+                    <Image src={"https://bit.ly/2Z4KKcF"***REMOVED*** alt={"image"***REMOVED*** borderRadius='lg'/>
+                </Box>
+            </Flex>
+
+
         </VStack>
     )
 ***REMOVED***
