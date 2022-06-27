b7f4b6114253ad961bacf057e924738cdaa1fadb
Slijeff <jeff83636tx@gmail.com>
2022-06-26 23:15:53 -0500

    feat: add photo display

--- photoly_frontend/components/Pathbar.jsx
+++ photoly_frontend/components/Pathbar.jsx
@@ -1,15 +1,33 @@
-import {Divider, Heading, HStack, Icon, Text, VStack} from "@chakra-ui/react";
+import {Box, Divider, Flex, HStack, Icon, Image, Text, VStack} from "@chakra-ui/react";
 import {BiChevronRight} from "react-icons/bi";
 
 export default function Pathbar(){
     return (
-        <VStack alignSelf={"flex-start"} pt={8} pl={8} w={"100%"} pr={8}>
+        <VStack pt={8} pl={8} w={"100%"} pr={8}>
             <HStack alignSelf={"flex-start"}>
                 <Icon as={BiChevronRight} boxSize={8} color={"gray.500"}/>
                 <Text fontWeight={"bold"} color={"gray.500"}>/home</Text>
             </HStack>
             <Divider/>
-            <Heading>Photo Here</Heading>
+            <Flex w={"100%"} justifyContent={"flex-start"} wrap={"wrap"}>
+                <Box maxW='sm' overflow='hidden' p={2}>
+                    <Image src={"https://bit.ly/2Z4KKcF"} alt={"image"} borderRadius='lg'/>
+                </Box>
+                <Box maxW='sm' overflow='hidden' p={2}>
+                    <Image src={"https://bit.ly/2Z4KKcF"} alt={"image"} borderRadius='lg'/>
+                </Box>
+                <Box maxW='sm' overflow='hidden' p={2}>
+                    <Image src={"https://bit.ly/2Z4KKcF"} alt={"image"} borderRadius='lg'/>
+                </Box>
+                <Box maxW='sm' overflow='hidden' p={2}>
+                    <Image src={"https://bit.ly/2Z4KKcF"} alt={"image"} borderRadius='lg'/>
+                </Box>
+                <Box maxW='sm' overflow='hidden' p={2}>
+                    <Image src={"https://bit.ly/2Z4KKcF"} alt={"image"} borderRadius='lg'/>
+                </Box>
+            </Flex>
+
+
         </VStack>
     )
 }
--- photoly_frontend/pages/home/index.jsx
+++ photoly_frontend/pages/home/index.jsx
@@ -1,6 +1,6 @@
 import Navbar from "../../components/Navbar"
 import Sidebar from "../../components/Sidebar";
-import {Heading, HStack} from "@chakra-ui/react";
+import {HStack} from "@chakra-ui/react";
 import Pathbar from "../../components/Pathbar";
 
 export default function Home() {
@@ -8,7 +8,7 @@ export default function Home() {
     return (
         <>
             <Navbar></Navbar>
-            <HStack>
+            <HStack alignItems={"flex-start"}>
                 <Sidebar/>
                 <Pathbar></Pathbar>
             </HStack>
