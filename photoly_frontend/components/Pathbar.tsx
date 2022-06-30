678dc6b544504f11f80974b3b521afd1b2feb8e3
Slijeff <jeff83636tx@gmail.com>
2022-06-29 21:37:16 -0500

    style: extract context menu logic into hook and modified code

--- photoly_frontend/components/Pathbar.tsx
+++ photoly_frontend/components/Pathbar.tsx
@@ -1,23 +1,12 @@
-import {Box, Divider, Flex, HStack, Icon, Image, Text, VStack***REMOVED*** from "@chakra-ui/react";
+import {Divider, HStack, Icon, Text, VStack***REMOVED*** from "@chakra-ui/react";
 import {BiChevronRight***REMOVED*** from "react-icons/bi";
-import {useCallback, useEffect, useState***REMOVED*** from "react";
-import ImageContext from "./ImageContext";
+import {ReactNode***REMOVED*** from "react";
 
-export default function Pathbar(){
-    const [show, setShow] = useState(false)
-    const [anchorPoint, setAnchorPoint] = useState({ x: 0, y: 0 ***REMOVED***)
-    const handleClick = useCallback(() => (show ? setShow(false) : null), [show])
-    const handleContextMenu = (e: MouseEvent) => {
-        e.preventDefault()
-        setAnchorPoint({ x: e.pageX, y: e.pageY ***REMOVED***)
-        setShow(true)
+interface PathbarProps {
+    children?: ReactNode
 ***REMOVED***
-    useEffect(() => {
-        document.addEventListener("click", handleClick);
-        return () => {
-            document.removeEventListener("click", handleClick);
-***REMOVED***
-***REMOVED***)
+export default function Pathbar({children***REMOVED***: PathbarProps){
+
     return (
         <VStack pt={8***REMOVED*** pl={8***REMOVED*** w={"100%"***REMOVED*** pr={8***REMOVED***>
             <HStack alignSelf={"flex-start"***REMOVED***>
@@ -25,15 +14,7 @@ export default function Pathbar(){
                 <Text fontWeight={"bold"***REMOVED*** color={"gray.500"***REMOVED***>/</Text>
             </HStack>
             <Divider/>
-            <Flex w={"100%"***REMOVED*** justifyContent={"flex-start"***REMOVED*** wrap={"wrap"***REMOVED***>
-                <Box maxW='sm' overflow='hidden' p={2***REMOVED*** onContextMenu={() => handleContextMenu***REMOVED***>
-                    <Image src={"https://bit.ly/2Z4KKcF"***REMOVED*** alt={"image"***REMOVED*** borderRadius='lg'/>
-                </Box>
-                <Box maxW='sm' overflow='hidden' p={2***REMOVED*** onContextMenu={() => handleContextMenu***REMOVED***>
-                    <Image src={"https://bit.ly/2Z4KKcF"***REMOVED*** alt={"image"***REMOVED*** borderRadius='lg'/>
-                </Box>
-            </Flex>
-   ***REMOVED*****REMOVED***show && <ImageContext x={anchorPoint.x***REMOVED*** y={anchorPoint.y***REMOVED***/>***REMOVED***
+   ***REMOVED*****REMOVED***children***REMOVED***
         </VStack>
     )
 ***REMOVED***
