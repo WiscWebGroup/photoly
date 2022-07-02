a2c6a352d83f900edd7c991c3cfefeb6f81722f0
Slijeff <jeff83636tx@gmail.com>
2022-07-02 12:34:51 -0500

    feat: add folder view

--- photoly_frontend/components/Images.tsx
+++ photoly_frontend/components/Images.tsx
@@ -1,14 +1,31 @@
-import {Box, Flex, Image***REMOVED*** from "@chakra-ui/react";
+import {Box, Button, Flex, Image, Text***REMOVED*** from "@chakra-ui/react";
 import ImageContextMenu from "./ImageContextMenu";
 import {useContextMenu***REMOVED*** from "../hooks/useContextMenu";
 import React from "react";
 import ImageView from "./ImageView";
+import {AiOutlineFolderOpen***REMOVED*** from 'react-icons/ai'
 
 const Images: React.FC = () => {
     const [showContext, handleContextMenu, anchorPoint] = useContextMenu()
     const [isModalOpen, setIsModalOpen] = React.useState(false)
     return (
         <>
+            <Text alignSelf={"flex-start"***REMOVED*** fontSize={"xl"***REMOVED*** fontWeight={"semibold"***REMOVED*** pl={2***REMOVED***>Folders</Text>
+            <Flex w={"100%"***REMOVED*** justifyContent={"flex-start"***REMOVED*** wrap={"wrap"***REMOVED***>
+                <Button leftIcon={<AiOutlineFolderOpen />***REMOVED*** colorScheme='gray' variant='solid' ml={2***REMOVED***>
+                    Folder1
+                </Button>
+                <Button leftIcon={<AiOutlineFolderOpen />***REMOVED*** colorScheme='gray' variant='solid' ml={2***REMOVED***>
+                    Folder2
+                </Button>
+                <Button leftIcon={<AiOutlineFolderOpen />***REMOVED*** colorScheme='gray' variant='solid' ml={2***REMOVED***>
+                    Folder3
+                </Button>
+                <Button leftIcon={<AiOutlineFolderOpen />***REMOVED*** colorScheme='gray' variant='solid' ml={2***REMOVED***>
+                    Folder4
+                </Button>
+            </Flex>
+            <Text alignSelf={"flex-start"***REMOVED*** fontSize={"xl"***REMOVED*** fontWeight={"semibold"***REMOVED*** pl={2***REMOVED***>Images</Text>
             <Flex w={"100%"***REMOVED*** justifyContent={"flex-start"***REMOVED*** wrap={"wrap"***REMOVED***>
                 <Box maxW='sm' overflow='hidden' p={2***REMOVED*** onContextMenu={handleContextMenu***REMOVED***>
                     <Image src={"https://bit.ly/2Z4KKcF"***REMOVED*** alt={"image"***REMOVED*** borderRadius='lg' onClick={() => setIsModalOpen(true)***REMOVED***/>
--- photoly_frontend/jest.config.js
+++ photoly_frontend/jest.config.js
@@ -9,10 +9,11 @@ const createJestConfig = nextJest({
 // Add any custom config to be passed to Jest
 const customJestConfig = {
     // Add more setup options before each test is run
-    // setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+    setupFilesAfterEnv: ['<rootDir>/setupTests.ts'],
     // if using TypeScript with a baseUrl set to the root directory then you need the below for alias' to work
     moduleDirectories: ['node_modules', '<rootDir>/'],
     testEnvironment: 'jest-environment-jsdom',
+
 ***REMOVED***
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
