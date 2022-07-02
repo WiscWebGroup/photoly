1ea8b779d52f2fe9e5341da7074fe6dfa845255b
Slijeff <jeff83636tx@gmail.com>
2022-07-02 12:34:51 -0500

    feat: add folder view

--- photoly_frontend/__tests__/TagItem.test.tsx
+++ photoly_frontend/__tests__/TagItem.test.tsx
@@ -2,6 +2,7 @@ import {describe, expect***REMOVED*** from "@jest/globals";
 import {render***REMOVED*** from "@testing-library/react";
 import TagItem from "../components/TagItem";
 
+
 describe("<TagItem/> renders correctly", () => {
     test("should render tag item on the screen", () => {
         const {container, getByText***REMOVED*** = render(<TagItem tagName={"something"***REMOVED***/>)
