e86233d02d2791640d2db70784a373efe02586bc
Slijeff <jeff83636tx@gmail.com>
2022-07-06 15:05:17 -0700

    refactor: remove inappropriate tags

--- photoly_frontend/components/ImageItem.tsx
+++ photoly_frontend/components/ImageItem.tsx
@@ -11,7 +11,7 @@ const ImageItem = (props: Exclude<ImageProps, "onClick" | "borderRadius">) => {
         >
         ***REMOVED***ref => (
                 <Box ref={ref***REMOVED*** maxW='sm' overflow='hidden' position='relative' p={2***REMOVED***>
-                    <Image {...props***REMOVED*** borderRadius='lg' onClick={onOpen***REMOVED***/>
+                    <Image {...props***REMOVED*** borderRadius='lg' onClick={onOpen***REMOVED*** alt={"image"***REMOVED***/>
                     <ImageView isViewOpen={isOpen***REMOVED*** onViewClose={onClose***REMOVED***/>
                 </Box>
             )***REMOVED***
