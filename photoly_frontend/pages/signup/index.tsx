9a50797f689c9e9701ea89af18661abe35707aaa
Slijeff <jeff83636tx@gmail.com>
2022-07-09 17:07:46 -0700

    feat: change register request to async; format signup stateful logic

--- photoly_frontend/pages/signup/index.tsx
+++ photoly_frontend/pages/signup/index.tsx
@@ -9,29 +9,35 @@ import {
     useToast,
     VStack
 } from "@chakra-ui/react";
-import React, {ChangeEvent, useState} from "react";
-import {useRouter} from "next/router";
+import React from "react";
 import axios from "axios";
 import useLocalStorage, {TOKEN_KEY} from "../../hooks/useLocalStorage";
+import useLoginForm from "../../hooks/useLoginForm";
 
 const Login: React.FC = () => {
+    const [
+        username,
+        email,
+        password,
+        handleUsernameChange,
+        handleEmailChange,
+        handlePasswordChange,
+        router,
+        isLoading,
+        setLoading
+    ] = useLoginForm()
     const [show, setShow] = useBoolean(false)
-    const [username, setUsername] = useState<string | undefined>()
-    const [email, setEmail] = useState<string | undefined>()
-    const [password, setPassword] = useState<string | undefined>()
-    const handleUsernameChange = (event: ChangeEvent<HTMLInputElement>) => setUsername(event.currentTarget.value)
-    const handleEmailChange = (event: ChangeEvent<HTMLInputElement>) => setEmail(event.currentTarget.value)
-    const handlePasswordChange = (event: ChangeEvent<HTMLInputElement>) => setPassword(event.currentTarget.value)
-    const router = useRouter()
     const toast = useToast()
     const {set} = useLocalStorage(TOKEN_KEY)
 
-    const submitSignUp = () => {
-        axios.post("/user/signUp", {
+    const submitSignUp = async () => {
+        setLoading.on()
+        await axios.post("/user/signUp", {
             userName: username,
             email: email,
             password: password
         }).then(res => {
+            setLoading.off()
             if (res.data.msgCode !== 200) {
                 toast({title: res.data.t, status: "error", isClosable: true, position: "top"})
             } else {
@@ -51,11 +57,13 @@ const Login: React.FC = () => {
                 <VStack rounded="2xl" boxShadow="2xl" padding={8} spacing={4} w={{base: "80vw", md: "30vw"}}
                         justify="space-evenly">
                     <Text fontSize="3xl" color="gray.700">Sign Up to Photoly</Text>
-                    <Input variant="outline" placeholder="Enter username" onChange={handleUsernameChange}></Input>
-                    <Input variant="outline" placeholder="Enter email" onChange={handleEmailChange}></Input>
+                    <Input variant="outline" placeholder="Enter username" onChange={handleUsernameChange}
+                           isDisabled={isLoading}></Input>
+                    <Input variant="outline" placeholder="Enter email" onChange={handleEmailChange}
+                           isDisabled={isLoading}></Input>
                     <InputGroup>
                         <Input variant="outline" type={show ? 'text' : 'password'} placeholder="Enter password"
-                               onChange={handlePasswordChange}></Input>
+                               onChange={handlePasswordChange} isDisabled={isLoading}></Input>
                         <InputRightElement pr={1}>
                             <Button padding={4} size='sm' onClick={setShow.toggle} fontSize="xs">
                                 {show ? 'Hide' : 'Show'}
@@ -63,7 +71,8 @@ const Login: React.FC = () => {
                         </InputRightElement>
                     </InputGroup>
 
-                    <Button colorScheme="teal" w="100%" onClick={submitSignUp}>Register</Button>
+                    <Button colorScheme="teal" w="100%" onClick={submitSignUp} isLoading={isLoading}
+                            loadingText={"Submitting"}>Register</Button>
                     <Button variant={"link"} onClick={() => router.push("/login")}>Already have an account?
                         Login</Button>
                 </VStack>
