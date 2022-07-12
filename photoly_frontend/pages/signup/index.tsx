82e55fd6b6b180b3ac9452e49b2207f53a97c213
Slijeff <jeff83636tx@gmail.com>
2022-07-12 10:27:46 -0700

    fix: modify signUp to use updated useApi

--- photoly_frontend/pages/signup/index.tsx
+++ photoly_frontend/pages/signup/index.tsx
@@ -24,7 +24,7 @@ const Login: React.FC = () => {
             email: email,
             password: password
 ***REMOVED***).then(res => {
-            if (!!res.data.token) {
+            if (!!res) {
                 set(res.data.token)
                 router.push("/home")
     ***REMOVED***
@@ -47,7 +47,7 @@ const Login: React.FC = () => {
                                onChange={handlePasswordChange***REMOVED*** isDisabled={isLoading***REMOVED***></Input>
                         <InputRightElement pr={1***REMOVED***>
                             <Button padding={4***REMOVED*** size='sm' onClick={setShow.toggle***REMOVED*** fontSize="xs">
-                            ***REMOVED***show ? 'Hide' : 'Show'***REMOVED***
+             ***REMOVED*****REMOVED*****REMOVED*****REMOVED***show ? 'Hide' : 'Show'***REMOVED***
                             </Button>
                         </InputRightElement>
                     </InputGroup>
