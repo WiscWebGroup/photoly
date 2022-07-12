d28dc4faa6a0860ed6ed3663983d6769824fdbcb
Slijeff <jeff83636tx@gmail.com>
2022-07-12 15:45:55 -0700

    feat: reformat manage page

--- photoly_frontend/components/ChangeInfoDrawer.tsx
+++ photoly_frontend/components/ChangeInfoDrawer.tsx
@@ -14,21 +14,25 @@ import useApi from "../hooks/useApi";
 import useToken from "../hooks/useToken";
 import Router from "next/router"
 
-interface NameAndAvatarDrawerProps {
+interface ChangeInfoProps {
     isOpen: boolean,
     onClose: () => void
 ***REMOVED***
 
-const NameAndAvatarDrawer: React.FC<NameAndAvatarDrawerProps> = ({isOpen, onClose***REMOVED***) => {
+const ChangeInfoDrawer: React.FC<ChangeInfoProps> = ({isOpen, onClose***REMOVED***) => {
     const [file, setFile] = useState<File>()
     const [username, setUsername] = useState<string>()
+    const [email, setEmail] = useState<string>()
     const token = useToken()
-    const {post***REMOVED*** = useApi()
+    const {post, isLoading***REMOVED*** = useApi()
     const handleFileChange = (e: ChangeEvent<HTMLInputElement>) => {
         if (!!e.target.files) setFile(e.target.files[0])
 ***REMOVED***
     const handleUsernameChange = (e: ChangeEvent<HTMLInputElement>) => {
         setUsername(e.target.value)
+***REMOVED***
+    const handleEmailChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setEmail(e.target.value)
 ***REMOVED***
     const handleSubmit = async () => {
         if (!!file){
@@ -48,7 +52,14 @@ const NameAndAvatarDrawer: React.FC<NameAndAvatarDrawerProps> = ({isOpen, onClos
    ***REMOVED*****REMOVED***
     ***REMOVED***)
 ***REMOVED***
-        Router.reload()
+        if (!!email) {
+            await post(`/user/updateEmail?email=${email***REMOVED***`, {***REMOVED***, {
+                headers: {
+                    "HRD-token": token
+   ***REMOVED*****REMOVED***
+    ***REMOVED***)
+***REMOVED***
+        if (!!email || !!username || !!file) Router.reload()
 ***REMOVED***
 
     return (
@@ -60,9 +71,10 @@ const NameAndAvatarDrawer: React.FC<NameAndAvatarDrawerProps> = ({isOpen, onClos
             <DrawerOverlay/>
             <DrawerContent>
                 <DrawerCloseButton/>
-                <DrawerHeader>Change Username & Avatar</DrawerHeader>
+                <DrawerHeader>Change Info</DrawerHeader>
                 <DrawerBody>
                     <Input placeholder='Username' onChange={handleUsernameChange***REMOVED***/>
+                    <Input placeholder='Email' mt={4***REMOVED*** onChange={handleEmailChange***REMOVED***/>
                     <Input type={"file"***REMOVED*** mt={4***REMOVED*** accept={"image/png, image/jpeg"***REMOVED*** onChange={handleFileChange***REMOVED***/>
                 </DrawerBody>
 
@@ -73,11 +85,11 @@ const NameAndAvatarDrawer: React.FC<NameAndAvatarDrawerProps> = ({isOpen, onClos
                     <Button colorScheme='blue' onClick={() => {
                         handleSubmit();
                         onClose()
-  ***REMOVED*****REMOVED*****REMOVED******REMOVED***>Save</Button>
+  ***REMOVED*****REMOVED*****REMOVED******REMOVED*** isLoading={isLoading***REMOVED*** loadingText={"Saving..."***REMOVED***>Save</Button>
                 </DrawerFooter>
             </DrawerContent>
         </Drawer>
     )
 ***REMOVED***
 
-export default NameAndAvatarDrawer
+export default ChangeInfoDrawer
