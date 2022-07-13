d8a9d171a508e001fa3b97adfab30cce81699cf2
Slijeff <jeff83636tx@gmail.com>
2022-07-13 14:07:13 -0700

    feat: change style of folders and images

--- photoly_frontend/components/Images.tsx
+++ photoly_frontend/components/Images.tsx
@@ -1,4 +1,4 @@
-import {Button, Flex, Text***REMOVED*** from "@chakra-ui/react";
+import {Box, Button, Flex, Text***REMOVED*** from "@chakra-ui/react";
 import React from "react";
 import {AiOutlineFolderOpen***REMOVED*** from 'react-icons/ai'
 import ImageItem from "./ImageItem";
@@ -8,18 +8,21 @@ const Images: React.FC = () => {
         <>
             <Text alignSelf={"flex-start"***REMOVED*** fontSize={"xl"***REMOVED*** fontWeight={"semibold"***REMOVED*** pl={2***REMOVED***>Folders</Text>
             <Flex w={"100%"***REMOVED*** justifyContent={"flex-start"***REMOVED*** wrap={"wrap"***REMOVED***>
-                <Button leftIcon={<AiOutlineFolderOpen />***REMOVED*** colorScheme='gray' variant='solid' ml={2***REMOVED***>
-                    Folder1
-                </Button>
-                <Button leftIcon={<AiOutlineFolderOpen />***REMOVED*** colorScheme='gray' variant='solid' ml={2***REMOVED***>
-                    Folder2
-                </Button>
-                <Button leftIcon={<AiOutlineFolderOpen />***REMOVED*** colorScheme='gray' variant='solid' ml={2***REMOVED***>
-                    Folder3
-                </Button>
-                <Button leftIcon={<AiOutlineFolderOpen />***REMOVED*** colorScheme='gray' variant='solid' ml={2***REMOVED***>
-                    Folder4
-                </Button>
+                <Box w={64***REMOVED*** m={2***REMOVED***>
+                    <Button leftIcon={<AiOutlineFolderOpen />***REMOVED*** colorScheme='gray' variant='solid' w={"100%"***REMOVED***>
+                        Folder 1
+                    </Button>
+                </Box>
+                <Box w={64***REMOVED*** m={2***REMOVED***>
+                    <Button leftIcon={<AiOutlineFolderOpen />***REMOVED*** colorScheme='gray' variant='solid' w={"100%"***REMOVED***>
+                        Folder 2
+                    </Button>
+                </Box>
+                <Box w={64***REMOVED*** m={2***REMOVED***>
+                    <Button leftIcon={<AiOutlineFolderOpen />***REMOVED*** colorScheme='gray' variant='solid' w={"100%"***REMOVED***>
+                        Folder 3
+                    </Button>
+                </Box>
             </Flex>
             <Text alignSelf={"flex-start"***REMOVED*** fontSize={"xl"***REMOVED*** fontWeight={"semibold"***REMOVED*** pl={2***REMOVED***>Images</Text>
             <Flex w={"100%"***REMOVED*** justifyContent={"flex-start"***REMOVED*** wrap={"wrap"***REMOVED***>
--- photoly_frontend/next.config.js
+++ photoly_frontend/next.config.js
@@ -1,12 +1,12 @@
 ***REMOVED*** @type {import('next').NextConfig***REMOVED******REMOVED***
-const serverAddr = 'http://129.146.3.179:8088/'
+const serverAddr = 'http://129.146.3.179:8088'
 const nextConfig = {
   reactStrictMode: true,
   async rewrites() {
     return [
   ***REMOVED***
         source: '/user/:path*',
-        destination: `${serverAddr***REMOVED***user/:path*`
+        destination: `${serverAddr***REMOVED***/user/:path*`
   ***REMOVED***
     ]
   ***REMOVED***
