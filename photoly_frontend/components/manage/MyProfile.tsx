29933d286c44a47468b4abdc443d12f325125ca0
Ye <Yixuan.Ye@Dell.com>
2022-07-14 16:37:14 -0500

    feat: tag and gallery 80% functionality

--- photoly_frontend/components/manage/MyProfile.tsx
+++ photoly_frontend/components/manage/MyProfile.tsx
@@ -127,6 +127,7 @@ const MyProfile: React.FC = () => {
         title: `Delete Successful`,
         status: "success",
         isClosable: true,
+        position: "top",
         duration: 3000,
   ***REMOVED***);
       getCred();
@@ -164,6 +165,7 @@ const MyProfile: React.FC = () => {
         status: "success",
         isClosable: true,
         duration: 3000,
+        position: "top",
   ***REMOVED***);
       getCred();
 ***REMOVED***
@@ -185,6 +187,7 @@ const MyProfile: React.FC = () => {
         status: "success",
         isClosable: true,
         duration: 3000,
+        position: "top",
   ***REMOVED***);
       getCred();
 ***REMOVED***
@@ -197,6 +200,7 @@ const MyProfile: React.FC = () => {
       status: "success",
       isClosable: true,
       duration: 3000,
+      position: "top",
 ***REMOVED***);
   ***REMOVED***;
 
@@ -392,7 +396,7 @@ const MyProfile: React.FC = () => {
                       <Tr key={cred.credId***REMOVED***>
                         <Td>{cred.credId***REMOVED***</Td>
                         <Td>
-  ***REMOVED*****REMOVED*****REMOVED*****REMOVED*****REMOVED***cred.authorization
+       ***REMOVED*****REMOVED*****REMOVED*****REMOVED***cred.authorization
                             .replace("C", "Upload ")
                             .replace("R", "Read ")
                             .replace("D", "Delete")***REMOVED***
