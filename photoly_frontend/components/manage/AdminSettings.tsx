9ea79a797366cefa72d3c64bac225f9761c41676
Harold <68500948+Harold-y@users.noreply.github.com>
2022-07-18 10:50:26 -0500

    feat: add param

--- photoly_frontend/components/manage/AdminSettings.tsx
+++ photoly_frontend/components/manage/AdminSettings.tsx
@@ -22,6 +22,11 @@ import useToken from "../../hooks/useToken";
 import useApi from "../../hooks/useApi";
 
 const AdminSettings: React.FC = () => {
+  const [mySQLStatus, setMySQLStatus] = React.useState<string>("");
+  const [redisStatus, setRedisStatus] = React.useState<string>("");
+
+  const [parameterRes, setParameterRes] = React.useState<Map<string,string>>();
+
   return (
     <>
       <Center h="calc(100%-4rem)" w={"85vw"}>
