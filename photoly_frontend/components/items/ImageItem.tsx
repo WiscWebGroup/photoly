e4a87e07030cefeb4c770b362d54b825f51ceaed
NataRich <natarichjay@gmail.com>
2022-08-07 22:20:39 -0500

    feat: add image view

--- photoly_frontend/components/items/ImageItem.tsx
+++ photoly_frontend/components/items/ImageItem.tsx
@@ -1,10 +1,18 @@
 import {Box, Image, ImageProps, useDisclosure, Text, Center***REMOVED*** from "@chakra-ui/react"
 import { ContextMenu ***REMOVED*** from "../ContextMenu"
 import ImageContextMenu from "../contextMenus/ImageContextMenu"
+import { useSearchData ***REMOVED*** from "../contexts/SearchContext"
 import ImageView from "../ImageView"
 
-const ImageItem = (props: Exclude<ImageProps, "onClick" | "borderRadius">) => {
+interface ImageItemProps {
+    pid: number
+    name: string
+    format: string
+***REMOVED***
+
+const ImageItem = (props: Exclude<ImageProps & ImageItemProps, "onClick" | "borderRadius">) => {
     const { isOpen, onOpen, onClose ***REMOVED*** = useDisclosure()
+    
 
     return (
         <ContextMenu<HTMLDivElement>
@@ -14,9 +22,16 @@ const ImageItem = (props: Exclude<ImageProps, "onClick" | "borderRadius">) => {
    ***REMOVED*****REMOVED***ref => (
                 <Box ref={ref***REMOVED*** w={64***REMOVED*** overflow='hidden' position='relative' m={2***REMOVED*** rounded={"md"***REMOVED*** border={"1px"***REMOVED***
                      borderColor={"gray.50"***REMOVED*** shadow={"md"***REMOVED***>
-                    <Image {...props***REMOVED*** onClick={onOpen***REMOVED*** alt={"image"***REMOVED***/>
+                    <Image 
+     ***REMOVED*****REMOVED*****REMOVED*****REMOVED***...props***REMOVED***
+                        w="100%" 
+                        h={128***REMOVED*** 
+                        objectFit="cover" 
+                        onClick={onOpen***REMOVED*** 
+                        alt={"image"***REMOVED***
+                    />
                     <Center bg={"white"***REMOVED***>
-                        <Text fontWeight={"hairline"***REMOVED***>filename.jpg</Text>
+                        <Text fontWeight={"hairline"***REMOVED***>{props.name + "." + props.format***REMOVED***</Text>
                     </Center>
                     <ImageView isViewOpen={isOpen***REMOVED*** onViewClose={onClose***REMOVED***/>
                 </Box>
