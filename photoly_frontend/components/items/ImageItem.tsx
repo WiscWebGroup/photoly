bdc100581008daf3523ba9da057927d4c5438b97
Slijeff <jeff83636tx@gmail.com>
2022-08-18 21:13:14 -0500

    feat: bind tags display with backend

--- photoly_frontend/components/items/ImageItem.tsx
+++ photoly_frontend/components/items/ImageItem.tsx
@@ -1,20 +1,47 @@
-import {Box, Image, ImageProps, useDisclosure, Text, Center***REMOVED*** from "@chakra-ui/react"
-import { ContextMenu ***REMOVED*** from "../ContextMenu"
+import {Box, Center, Image, ImageProps, Text, useDisclosure***REMOVED*** from "@chakra-ui/react"
+import {ContextMenu***REMOVED*** from "../ContextMenu"
 import ImageContextMenu from "../contextMenus/ImageContextMenu"
-import { useSearchData ***REMOVED*** from "../contexts/SearchContext"
 import ImageView from "../ImageView"
+import {useEffect, useState***REMOVED*** from "react";
+import useApi from "../../hooks/useApi";
+import useToken from "../../hooks/useToken";
 
 interface ImageItemProps {
     pid: number
     name: string
     format: string
-    uploadDate: string
-    orgSrc: string
+    uploaddate: string
+    orgsrc: string
+***REMOVED***
+interface TagLookup{
+    photo_id: number
+    tag_name: string
+    tag_id: number
 ***REMOVED***
 
 const ImageItem = (props: Exclude<ImageProps & ImageItemProps, "onClick" | "borderRadius">) => {
-    const { isOpen, onOpen, onClose ***REMOVED*** = useDisclosure()
-    
+    const {isOpen, onOpen, onClose***REMOVED*** = useDisclosure()
+    const [tags, setTags] = useState<string[]>([]);
+    const {get***REMOVED*** = useApi()
+    const token = useToken()
+
+    useEffect(() => {
+        const getTags = async () => {
+            await get(`/photo/getTagByPhoto?photoId=${props.pid***REMOVED***`, {
+                headers: {"HRD-token": token***REMOVED***
+    ***REMOVED***).then((response) => {
+                if (!!response && response.data && response.data.msgCode === 200) {
+                    const temp: string[] = []
+                    response.data.t.forEach((item: TagLookup) => {
+                        temp.push(item.tag_name)
+  ***REMOVED*****REMOVED*****REMOVED***)
+                    setTags(temp)
+   ***REMOVED*****REMOVED***
+    ***REMOVED***)
+
+***REMOVED***
+        if (token) getTags()
+***REMOVED***, [token])
 
     return (
         <ContextMenu<HTMLDivElement>
@@ -24,19 +51,20 @@ const ImageItem = (props: Exclude<ImageProps & ImageItemProps, "onClick" | "bord
    ***REMOVED*****REMOVED***ref => (
                 <Box ref={ref***REMOVED*** w={64***REMOVED*** overflow='hidden' position='relative' m={2***REMOVED*** rounded={"md"***REMOVED*** border={"1px"***REMOVED***
                      borderColor={"gray.50"***REMOVED*** shadow={"md"***REMOVED***>
-                    <Image 
+                    <Image
      ***REMOVED*****REMOVED*****REMOVED*****REMOVED***...props***REMOVED***
-                        w="100%" 
-                        h={128***REMOVED*** 
-                        objectFit="cover" 
-                        onClick={onOpen***REMOVED*** 
+                        w="100%"
+                        h={128***REMOVED***
+                        objectFit="cover"
+                        onClick={onOpen***REMOVED***
                         alt={"image"***REMOVED***
-                        
+
                     />
                     <Center bg={"white"***REMOVED***>
                         <Text fontWeight={"hairline"***REMOVED***>{props.name + "." + props.format***REMOVED***</Text>
                     </Center>
-                    <ImageView isViewOpen={isOpen***REMOVED*** onViewClose={onClose***REMOVED*** path={props.src***REMOVED*** pname={props.name***REMOVED*** date={props.uploadDate***REMOVED*** orgSrc={props.orgSrc***REMOVED***/>
+                    <ImageView isViewOpen={isOpen***REMOVED*** onViewClose={onClose***REMOVED*** path={props.src***REMOVED*** pname={props.name***REMOVED***
+                               date={props.uploaddate***REMOVED*** orgsrc={props.orgsrc***REMOVED*** tags={tags***REMOVED*** albums={[]***REMOVED***/>
                 </Box>
             )***REMOVED***
         </ContextMenu>
